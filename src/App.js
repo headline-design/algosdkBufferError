50ff0942dea6c3365bb1bee90cc182b2514af5c7
headline-dev <aaron@headline-inc.com>
2023-01-11 22:46:57 +0000

    working test for algosdk encodeUint64 and Buffer errors

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import algosdk from 'algosdk'
 
-console.log(algosdk.encodeUint64(123456))
+
 
 function App() {
+
+  function test() {
+    // note: upgrading to react-scripts > 5 in package.json causes error to change to "Buffer is not defined"
+    try {
+      console.log(algosdk.encodeUint64(123456));
+    } catch (e) {
+      console.log(e);
+      alert(e);
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={test} >Test Algosdk encodeUint64 error</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
